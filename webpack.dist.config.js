5fadb4e85de5e48eef5f9349eab6c4b754279258
patw <patw.hi@gmail.com>
2015-10-06 14:44:46 +0800

    Use pngquant to generate smaller lossy images

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -83,7 +83,7 @@ module.exports = {
       test: /\.(jpe?g|png|gif|svg)$/i,
       loaders: [
         'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        'image-webpack?{progressive:true, optimizationLevel: 3, interlaced: false, pngquant:{quality: "30-40", speed: 1}}'
       ]
     }]
   },
