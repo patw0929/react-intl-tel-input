388522a82aedc69c7e076e1f9d640cfe921b7a8d
patw <patw.hi@gmail.com>
2015-11-07 15:44:46 +0800

    Fix ajax & executing script issue of IE9

--- src/components/IntlTelInput.js
+++ src/components/IntlTelInput.js
@@ -554,11 +554,14 @@ export default React.createClass({
     let ajax = new Ajax({
       url: this.props.utilsScript
     }).on('success', (event) => {
-      let data = event.target.response;
+      let data = event.target.responseText;
+
       if (data) {
-        (window.execScript || function (res) {
-          window.eval.call(window, res);
-        })(data);
+        if (window.execScript) {
+          window.execScript(data);
+        } else {
+          window.eval(data);
+        }
       }
       this.utilsScriptDeferred.resolve();
     });
