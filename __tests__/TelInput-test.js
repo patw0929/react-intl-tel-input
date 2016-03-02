cb39afa8777d75abe0fd71323b59cd7bcf53300f
patw <patw.hi@gmail.com>
2016-03-02 14:26:59 +0800

    Removed the id attribute of test component

--- __tests__/TelInput-test.js
+++ __tests__/TelInput-test.js
@@ -10,7 +10,7 @@ describe('TelInput', () => {
 
   beforeEach(() => {
     component = TestUtils.renderIntoDocument(
-        <IntlTelInput id={'0'} css={['intl-tel-input', 'form-control phoneNumber']}
+        <IntlTelInput css={['intl-tel-input', 'form-control phoneNumber']}
           fieldName={'telephone'}
           defaultValue={'0999 123 456'}
           utilsScript={'../example/assets/libphonenumber.js'}
