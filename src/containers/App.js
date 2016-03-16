5f233f2989bb1daf79fc6bfa935bebb622d0d707
patw <patw.hi@gmail.com>
2016-03-16 11:09:48 +0800

    Removed Provider & store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import IntlTelInputApp from './IntlTelInputApp';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import * as reducers from '../reducers';
 import '../styles/intlTelInput.scss';
-
-const reducer = combineReducers(reducers);
-let devtool = window.devToolsExtension ? window.devToolsExtension() : undefined;
-if (process.env.NODE_ENV === 'production') {
-  devtool = undefined;
-}
 
 class IntlTelInput extends Component {
   static propTypes = {
@@ -36,35 +27,31 @@ class IntlTelInput extends Component {
     onSelectFlag: PropTypes.func,
   }
 
-  store = createStore(reducer, {}, devtool);
-
   render() {
     return (
-      <Provider store={this.store}>
-        <IntlTelInputApp
-          value={this.props.value}
-          countriesData={this.props.countriesData}
-          defaultValue={this.props.defaultValue}
-          disabled={this.props.disabled}
-          onPhoneNumberChange={this.props.onPhoneNumberChange}
-          onSelectFlag={this.props.onSelectFlag}
-          css={this.props.css}
-          fieldName={this.props.fieldName}
-          allowExtensions={this.props.allowExtensions}
-          autoFormat={this.props.autoFormat}
-          autoPlaceholder={this.props.autoPlaceholder}
-          preprocessPlaceholder={this.props.preprocessPlaceholder}
-          autoHideDialCode={this.props.autoHideDialCode}
-          defaultCountry={this.props.defaultCountry}
-          geoIpLookup={this.props.geoIpLookup}
-          nationalMode={this.props.nationalMode}
-          numberType={this.props.numberType}
-          noCountryDataHandler={this.props.noCountryDataHandler}
-          onlyCountries={this.props.onlyCountries}
-          preferredCountries={this.props.preferredCountries}
-          utilsScript={this.props.utilsScript}
-        />
-      </Provider>
+      <IntlTelInputApp
+        value={this.props.value}
+        countriesData={this.props.countriesData}
+        defaultValue={this.props.defaultValue}
+        disabled={this.props.disabled}
+        onPhoneNumberChange={this.props.onPhoneNumberChange}
+        onSelectFlag={this.props.onSelectFlag}
+        css={this.props.css}
+        fieldName={this.props.fieldName}
+        allowExtensions={this.props.allowExtensions}
+        autoFormat={this.props.autoFormat}
+        autoPlaceholder={this.props.autoPlaceholder}
+        autoHideDialCode={this.props.autoHideDialCode}
+        defaultCountry={this.props.defaultCountry}
+        geoIpLookup={this.props.geoIpLookup}
+        nationalMode={this.props.nationalMode}
+        numberType={this.props.numberType}
+        noCountryDataHandler={this.props.noCountryDataHandler}
+        onlyCountries={this.props.onlyCountries}
+        preferredCountries={this.props.preferredCountries}
+        preprocessPlaceholder={this.props.preprocessPlaceholder}
+        utilsScript={this.props.utilsScript}
+      />
     );
   }
 }
