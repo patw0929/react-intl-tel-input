6ceb089b6fb0b623300c6aa17247b24cdb29ce3a
patw <patw.hi@gmail.com>
2016-06-16 14:17:54 +0800

    RootModal component for country list on mobile

---
+++ src/components/RootModal.js
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+class RootModal extends Component {
+  static propTypes = {
+    children: PropTypes.object,
+  };
+
+  componentDidMount() {
+    this.modalTarget = document.createElement('div');
+    this.modalTarget.className = 'intl-tel-input iti-container';
+    document.body.appendChild(this.modalTarget);
+    this._render();
+  }
+
+  componentWillUpdate() {
+    this._render();
+  }
+
+  componentWillUnmount() {
+    ReactDOM.unmountComponentAtNode(this.modalTarget);
+    document.body.removeChild(this.modalTarget);
+  }
+
+  _render() {
+    ReactDOM.render(
+      <div>{this.props.children}</div>,
+      this.modalTarget
+    );
+  }
+
+  render() {
+    return <noscript />;
+  }
+}
+
+export default RootModal;
