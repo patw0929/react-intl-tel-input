92b4915ba9df9caeb0070e22b72148c511675747
patw <patw.hi@gmail.com>
2018-12-16 11:47:54 +0800

    Fixed example.js

--- src/example.js
+++ src/example.js
@@ -2,28 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import IntlTelInput from 'react-intl-tel-input'; // eslint-disable-line import/no-extraneous-dependencies
 
-// import 'file-loader?name=libphonenumber.js!./libphonenumber.js';
-// import './main.css';
-
-const loadJSONP = (url, callback) => {
-  const ref = window.document.getElementsByTagName('script')[0];
-  const script = window.document.createElement('script');
-
-  script.src = `${url +
-    (url.indexOf('?') + 1 ? '&' : '?')}callback=${callback}`;
-  ref.parentNode.insertBefore(script, ref);
-  script.onload = () => {
-    script.remove();
-  };
-};
-
 const lookup = callback => {
-  loadJSONP('http://ipinfo.io', 'sendBack');
-  window.sendBack = resp => {
-    const countryCode = resp && resp.country ? resp.country : '';
+  const request = new XMLHttpRequest();
 
-    callback(countryCode);
-  };
+  request.addEventListener('load', () => {
+    callback(JSON.parse(request.responseText).country);
+  });
+
+  request.open('GET', 'https://ipapi.co/json/');
+  request.send();
 };
 
 function log(...args) {
