f9bcb68582571870f0da4f84a66140e56d460116
Levy Xu <levy@LevyMac.local>
2019-01-18 11:41:58 +0800

    update cursor position after focused

--- src/components/TelInput.js
+++ src/components/TelInput.js
@@ -19,17 +19,35 @@ export default class TelInput extends Component {
     cursorPosition: PropTypes.number,
   };
 
+  state = {
+    hasFocus: false,
+  }
+
   componentDidUpdate() {
-    this.tel.setSelectionRange(
-      this.props.cursorPosition,
-      this.props.cursorPosition
-    );
+    if (this.state.hasFocus) {
+      this.tel.setSelectionRange(
+        this.props.cursorPosition,
+        this.props.cursorPosition
+      );
+    }
   }
 
   refHandler = element => {
     this.tel = element;
     this.props.refCallback(element);
   };
+
+  inputOnBlur = () => {
+    this.setState({ hasFocus: false });
+
+    if (this.props.handleOnBlur) {
+      this.props.handleOnBlur();
+    }
+  }
+
+  inputOnFocus = () => {
+    this.setState({ hasFocus: true });
+  }
 
   render() {
     return (
@@ -46,7 +64,8 @@ export default class TelInput extends Component {
         value={this.props.value}
         placeholder={this.props.placeholder}
         onChange={this.props.handleInputChange}
-        onBlur={this.props.handleOnBlur}
+        onBlur={this.inputOnBlur}
+        onFocus={this.inputOnFocus}
         autoFocus={this.props.autoFocus}
       />
     );
