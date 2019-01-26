2d3f40bfcc7eebf252d28f3e686903d99e0b1eaa
Farhad Yasir <farhad.yasir.nb@gmail.com>
2019-01-27 08:20:00 +1100

    feat(storybook): Setup storybook and add webpack config for storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,48 @@
+
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const paths = require('../config/paths');
+
+module.exports = {
+  devtool: false,
+  entry: {
+    main: '../src/components/IntlTelInputApp.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: paths.appSrc,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: paths.appSrc,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader?outputStyle=expanded',
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]',
+          publicPath: '/',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+};
+
