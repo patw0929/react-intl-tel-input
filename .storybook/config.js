7b5548d5c49c8cff7ba11d6f176239a15735d58d
patw <patw.hi@gmail.com>
2019-01-27 15:47:40 +0800

    chore(storybook): Tweaked storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,22 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { version } from '../package.json';
 
 const req = require.context('./stories', true, /.js*/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
+
+  require('./stories.scss');
 }
+
+addDecorator(
+  withOptions({
+    showAddonPanel: false,
+    name: `react-intl-tel-input v${version}`,
+    url: 'https://github.com/patw0929/react-intl-tel-input',
+    sidebarAnimations: true,
+  })
+);
 
 configure(loadStories, module);
