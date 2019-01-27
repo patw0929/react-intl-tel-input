0acdd43def3e12bbf015aa3eb01e44fe8cc7365a
patw <patw.hi@gmail.com>
2019-01-27 17:44:03 +0800

    Completed story of Basic

--- .storybook/stories/Basic/Basic.stories.js
+++ .storybook/stories/Basic/Basic.stories.js
@@ -1,39 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { xonokai } from 'react-syntax-highlighter/dist/styles/prism';
 
+import { log } from '../../helpers/helpers';
 import IntlTelInput from '../../../src/components/IntlTelInputApp';
 import '../../../example/main.css';
 
 const codeString = `\
-<IntlTelInput
-  defaultCountry="tw"
-  css={['intl-tel-input', 'form-control']}
-  format
-/>
+  state = {
+    value: '',
+  };
+
+  handleChange = (isValid, value, countryData, number, ext) => {
+    console.log(isValid, value, countryData, number, ext);
+
+    this.setState({
+      value,
+    });
+  };
+
+  render() {
+    return (
+      <IntlTelInput
+        defaultCountry="tw"
+        css={['intl-tel-input', 'form-control']}
+        value={this.state.value}
+        onPhoneNumberChange={this.handleChange}
+        format
+      />
+    );
+  }
 `;
 
-export default () => {
-  return (
-    <div className="story">
-      <h1 className="title">Basic Usage</h1>
 
-      <div className="demo">
-        <IntlTelInput
-          defaultCountry="tw"
-          css={['intl-tel-input', 'form-control']}
-          format
-        />
+export default class Basic extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = (isValid, value, countryData, number, ext) => {
+    log(isValid, value, countryData, number, ext);
+
+    this.setState({
+      value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="story">
+        <h1 className="title">Basic Usage</h1>
+
+        <div className="demo">
+          <IntlTelInput
+            defaultCountry="tw"
+            css={['intl-tel-input', 'form-control']}
+            value={this.state.value}
+            onPhoneNumberChange={this.handleChange}
+            format
+          />
+
+          <div className="preview">
+            Phone Number: {this.state.value}
+          </div>
+
+          <div
+            id="debug"
+            className="code pre-scrollable"
+          />
+        </div>
+
+        <h2>Code</h2>
+
+        <SyntaxHighlighter
+          language="jsx"
+          style={xonokai}
+        >
+          {codeString}
+        </SyntaxHighlighter>
       </div>
-
-      <h2>Code</h2>
-
-      <SyntaxHighlighter
-        language="jsx"
-        style={xonokai}
-      >
-        {codeString}
-      </SyntaxHighlighter>
-    </div>
-  );
+    );
+  }
 };
