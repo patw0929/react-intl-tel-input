7ac33fa885f91d67edcf68bad95507461d490216
patw <patw.hi@gmail.com>
2019-02-12 09:55:10 +0800

    fix(path): Fixed the public path of storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: 'img/[name].[ext]',
-          publicPath: '/',
+          publicPath: './',
         },
       },
       {
