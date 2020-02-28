f274666a6f2abd8639df96585858a1124588225e
Marc Cataford <marc.cataford@tophatmonocle.com>
2020-02-28 15:56:18 -0500

    wip: finishing the footprinting work

--- scripts/package-footprint.js
+++ scripts/package-footprint.js
@@ -1,72 +1,98 @@
-const path = require('path')
-const { spawnSync } = require('child_process')
-const { readFileSync, writeFileSync } = require('fs')
+const path = require('path');
+const { spawnSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+const chalk = require('chalk');
 
-const FOOTPRINT_SNAPSHOT = path.join(__dirname, '..', '.footprint.json')
+const FOOTPRINT_SNAPSHOT = path.join(__dirname, '..', '.footprint.json');
 
 function sizeStringToSize(sizeString) {
-  const sizeSuffix = /([A-Za-z]+)/.exec(sizeString)[1]
-  const sizeMagnitude = /([0-9.]+)/.exec(sizeString)[1]
+  const sizeSuffix = /([A-Za-z]+)/.exec(sizeString)[1];
+  const sizeMagnitude = /([0-9.]+)/.exec(sizeString)[1];
 
-  let multiplier = 1
+  let multiplier = 1;
 
-  if (sizeSuffix === 'kB')
-    multiplier = 1000
+  if (sizeSuffix === 'kB') multiplier = 1000;
 
-  return parseFloat(sizeMagnitude) * multiplier
+  return parseFloat(sizeMagnitude) * multiplier;
 }
 
 function getLastReportedFootprint() {
   try {
-    const previousFootprint = readFileSync(FOOTPRINT_SNAPSHOT, { encoding: 'utf-8' })
-    const parsedFootprint = JSON.parse(previousFootprint)
+    const previousFootprint = readFileSync(FOOTPRINT_SNAPSHOT, {
+      encoding: 'utf-8',
+    });
+    const parsedFootprint = JSON.parse(previousFootprint);
 
-        
-    return parsedFootprint
-  } catch(e) {
-    console.log('No footprint available')
-    
-    return {}
+    return parsedFootprint;
+  } catch (e) {
+    console.log('No footprint available');
+
+    return {};
   }
 }
 
 function getCurrentFootprint() {
-  const { stderr } = spawnSync('npm', ['pack', '--dry-run'], { encoding: 'utf-8', cwd: path.join(__dirname, '..') })
-  const packedSize = /package size:\s+([0-9.]+ \w+)/ig.exec(stderr)
-  const unpackedSize = /unpacked size:\s+([0-9.]+ \w+)/ig.exec(stderr)
+  const { stderr } = spawnSync('npm', ['pack', '--dry-run'], {
+    encoding: 'utf-8',
+    cwd: path.join(__dirname, '..'),
+  });
+  const packedSize = /package size:\s+([0-9.]+ \w+)/gi.exec(stderr);
+  const unpackedSize = /unpacked size:\s+([0-9.]+ \w+)/gi.exec(stderr);
 
-    
   return {
     packed: packedSize[1],
     unpacked: unpackedSize[1],
-  }
+  };
 }
 
 function writeNewFootprint({ packed, unpacked, limit }) {
-  writeFileSync(FOOTPRINT_SNAPSHOT, JSON.stringify({ packed, unpacked, limit }))
+  writeFileSync(
+    FOOTPRINT_SNAPSHOT,
+    JSON.stringify({ packed, unpacked, limit })
+  );
 }
 
-const { packed: newPacked, unpacked: newUnpacked } = getCurrentFootprint()
-const { packed, limit } = getLastReportedFootprint()
+const { packed: newPacked, unpacked: newUnpacked } = getCurrentFootprint();
+const { packed, limit } = getLastReportedFootprint();
 
 if (!limit) {
-  console.warn('No package size limit is defined! Nothing to compare against.')
-  console.log(`New package size: ${newPacked} (Unpacked: ${newUnpacked})`)
-  writeNewFootprint({ packed: newPacked, unpacked: newUnpacked, limit: newPacked })
-  console.log(`Wrote .footprint with limit = ${newPacked}`)
-  
-  return
+  console.warn('No package size limit is defined! Nothing to compare against.');
+  console.log(
+    chalk.bold(`New package size: ${newPacked} (Unpacked: ${newUnpacked})`)
+  );
+  writeNewFootprint({
+    packed: newPacked,
+    unpacked: newUnpacked,
+    limit: newPacked,
+  });
+  console.log(`Wrote .footprint with limit = ${newPacked}`);
+
+  process.exit(0);
 }
 
-const beforePacked = sizeStringToSize(packed)
-const afterPacked = sizeStringToSize(newPacked)
-const upperLimit = sizeStringToSize(limit)
+const beforePacked = sizeStringToSize(packed);
+const afterPacked = sizeStringToSize(newPacked);
+const upperLimit = sizeStringToSize(limit);
 
 if (beforePacked >= afterPacked && afterPacked <= upperLimit) {
-  console.log(`Package size comparison: ${packed} >= ${newPacked}, limit = ${limit}`)
+  console.log(
+    chalk.green(
+      `Package size comparison: ${packed} >= ${newPacked}, limit = ${limit}`
+    )
+  );
 } else if (beforePacked < afterPacked && afterPacked <= upperLimit) {
-  console.warn(`Package size comparison: ${packed} < ${newPacked}, limit = ${limit}`)
+  console.warn(
+    chalk.yellow(
+      `Package size comparison: ${packed} < ${newPacked}, limit = ${limit}`
+    )
+  );
 } else if (afterPacked > upperLimit) {
-  console.error(`Package size comparison: ${packed} < ${newPacked}, LIMIT EXCEEDED: ${newPacked} > ${limit}`)
-  console.error('Update the limit in .footprint.json or review your changes')
+  console.log(
+    chalk.red(
+      `Package size comparison: ${packed} < ${newPacked}, LIMIT EXCEEDED: ${newPacked} > ${limit}\nUpdate the limit in .footprint.json or review your changes`
+    )
+  );
+  process.exit(1);
 }
+
+process.exit(0);
