3964ba1df35cd634be4613836552cde50980a098
Marc Cataford <c.marcandre@gmail.com>
2020-02-29 23:23:48 -0500

    refactor: extract flagbox (#315)

---
+++ src/components/FlagBox.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const FlagBox = ({
+  dialCode,
+  isoCode,
+  name,
+  onMouseOver,
+  onFocus,
+  onClick,
+  flagRef,
+  innerFlagRef,
+  countryClass,
+}) => (
+  <li
+    className={countryClass}
+    data-dial-code={dialCode}
+    data-country-code={isoCode}
+    onMouseOver={onMouseOver}
+    onFocus={onFocus}
+    onClick={onClick}
+  >
+    <div
+      ref={flagRef}
+      className="flag-box"
+    >
+      <div
+        ref={innerFlagRef}
+        className={`iti-flag ${isoCode}`}
+      />
+    </div>
+
+    <span className="country-name">{name}</span>
+    <span className="dial-code">
+      {`+ ${dialCode}`}
+    </span>
+  </li>
+) 
+
+FlagBox.propTypes = {
+  dialCode: PropTypes.string.isRequired,
+  isoCode: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onMouseOver: PropTypes.func,
+  onFocus: PropTypes.func,
+  onClick: PropTypes.func,
+  flagRef: PropTypes.func,
+  innerFlagRef: PropTypes.func,
+  countryClass: PropTypes.string.isRequired,
+}
+
+FlagBox.defaultProps = {
+  onFocus: () => {},
+  onMouseOver: () => {},
+  onClick: () => {},
+}
+
+export default FlagBox
