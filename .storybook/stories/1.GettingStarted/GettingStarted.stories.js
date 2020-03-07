9444d23278816b68e0fa22cf3dcad9379a528cf5
Marc Cataford <c.marcandre@gmail.com>
2020-03-07 11:28:12 -0500

    Removing SCSS import from components (#322)
    * refactor: no scss imports in components
    
    * refactor: story includes scss imports

--- .storybook/stories/1.GettingStarted/GettingStarted.stories.js
+++ .storybook/stories/1.GettingStarted/GettingStarted.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 import IntlTelInput from '../../../src/components/IntlTelInput';
+import '../../../src/styles/intlTelInput.scss'
 
 storiesOf('Documentation', module)
   .addParameters({ options: { showAddonPanel: false } })
