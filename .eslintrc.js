ca4fe0aadd10327f8c949ad23ef8b30c24047478
Marc Cataford <c.marcandre@gmail.com>
2020-03-08 13:03:57 -0400

    chore: clean up eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ['eslint-config-airbnb', 'prettier'],
+    extends: ['eslint-config-airbnb', "plugin:prettier/recommended"],
+  plugins: ['react', 'import', 'security', 'prettier'],
   parser: 'babel-eslint',
   env: {
     browser: true,
@@ -8,12 +9,14 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'prettier/prettier': 'error',
     'prefer-destructuring': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/no-autofocus': 'off',
     'jsx-a11y/no-noninteractive-tabindex': 'off',
     'jsx-a11y/anchor-has-content': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
     'react/destructuring-assignment': 'off',
     'react/jsx-no-bind': 'error',
     'react/no-multi-comp': 'off',
@@ -23,14 +26,8 @@ module.exports = {
       'ForInStatement',
       'WithStatement',
     ],
-    'newline-after-var': ['error', 'always'],
-    'newline-before-return': 'error',
     'comma-dangle': ['error', 'always-multiline'], // https://github.com/airbnb/javascript/commit/788208295469e19b806c06e01095dc8ba1b6cdc9
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'no-console': 0,
-    'no-alert': 0,
     'no-underscore-dangle': 'off',
-    'max-len': 'off',
     'react/require-default-props': 'off',
     'react/jsx-curly-spacing': 'off',
     'arrow-body-style': 'off',
@@ -48,9 +45,6 @@ module.exports = {
     ],
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'react/no-string-refs': 'off',
-    'arrow-parens': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'react/prefer-stateless-function': 'off',
     'no-param-reassign': 'off',
     'no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'import/no-unresolved': [
@@ -62,7 +56,6 @@ module.exports = {
       'error',
       {
         devDependencies: [
-          'scripts/**/*.js',
           'test/**', // tape, common npm pattern
           'tests/**', // also common npm pattern
           'spec/**', // mocha, rspec-like pattern
@@ -80,30 +73,9 @@ module.exports = {
         optionalDependencies: false,
       },
     ],
-    indent: [
-      'error',
-      2,
-      {
-        SwitchCase: 1,
-        VariableDeclarator: 1,
-        outerIIFEBody: 1,
-        MemberExpression: 1,
-        // CallExpression: {
-        // parameters: null,
-        // },
-        FunctionDeclaration: {
-          parameters: 1,
-          body: 1,
-        },
-        FunctionExpression: {
-          parameters: 1,
-          body: 1,
-        },
-      },
-    ],
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
-  plugins: ['react', 'import', 'security'],
+  
   globals: {
     __DEVELOPMENT__: true,
     __CLIENT__: true,
