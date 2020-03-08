919cc17ae42727ff555cd307f001d12b0c62e96d
Marc Cataford <c.marcandre@gmail.com>
2020-03-08 14:21:10 -0400

    Fixing style exports (#328)
    * chore: flatting directory structure
    
    * fix: remove dependency on node-sass
    
    * chore: update storybook imports
    
    * docs: reset version, set contrib
    
    * docs: minor updates

--- .storybook/stories/2.Props/Props.stories.js
+++ .storybook/stories/2.Props/Props.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 import IntlTelInput from '../../../src/components/IntlTelInput';
-import '../../../src/styles/intlTelInput.scss'
+import '../../../src/intlTelInput.scss'
 storiesOf('Documentation', module)
   .addParameters({ options: { showAddonPanel: false } })
   .add('Props', withInfo({ inline: true, source: false })(() => <IntlTelInput />));
