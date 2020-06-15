b11191f8d984dd7aa231a6eacc3e7703d0e0022a
Marc Cataford <c.marcandre@gmail.com>
2020-06-15 08:58:35 -0400

    test: coverage for custom classes

---
+++ src/components/__tests__/IntlTelInput.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import IntlTelInput from '../IntlTelInput'
+
+describe('Style customization', () => {
+  it('correctly applies user-supplied classes on outer container', () => {
+    const component = shallow(<IntlTelInput />)
+    const mockClass = 'mock-class-1'
+    component.setProps({ containerClassName: mockClass })
+    expect(component.props().className).toMatchInlineSnapshot(
+      `"allow-dropdown mock-class-1"`
+    )
+    const otherMockClass = 'mock-class-2'
+    component.setProps({ containerClassName: otherMockClass })
+    expect(component.props().className).toMatchInlineSnapshot(
+      `"allow-dropdown mock-class-2"`
+    )
+  })
+})
